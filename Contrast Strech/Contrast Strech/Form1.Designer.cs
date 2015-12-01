bdd2a1136474bacf445126ecc7da4831052ea788
trademkose <tr.ademkose@gmail.com>
2015-12-01 15:25:39 +0200

    Image Processing
    Contrast Strech - Brightness increasing&decreasing

---
+++ Contrast Strech/Contrast Strech/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Contrast_Strech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox_odsu = new System.Windows.Forms.PictureBox();
+            this.pictureBox_orjinal = new System.Windows.Forms.PictureBox();
+            this.button_resimyukle = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_odsu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_orjinal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(412, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Histogram Strech";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Ham Görüntü - Orjinal Image";
+            // 
+            // pictureBox_odsu
+            // 
+            this.pictureBox_odsu.Location = new System.Drawing.Point(415, 33);
+            this.pictureBox_odsu.Name = "pictureBox_odsu";
+            this.pictureBox_odsu.Size = new System.Drawing.Size(386, 256);
+            this.pictureBox_odsu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox_odsu.TabIndex = 17;
+            this.pictureBox_odsu.TabStop = false;
+            // 
+            // pictureBox_orjinal
+            // 
+            this.pictureBox_orjinal.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_orjinal.Image")));
+            this.pictureBox_orjinal.Location = new System.Drawing.Point(12, 33);
+            this.pictureBox_orjinal.Name = "pictureBox_orjinal";
+            this.pictureBox_orjinal.Size = new System.Drawing.Size(386, 256);
+            this.pictureBox_orjinal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox_orjinal.TabIndex = 16;
+            this.pictureBox_orjinal.TabStop = false;
+            // 
+            // button_resimyukle
+            // 
+            this.button_resimyukle.Location = new System.Drawing.Point(811, 42);
+            this.button_resimyukle.Name = "button_resimyukle";
+            this.button_resimyukle.Size = new System.Drawing.Size(108, 23);
+            this.button_resimyukle.TabIndex = 20;
+            this.button_resimyukle.Text = "Load Image";
+            this.button_resimyukle.UseVisualStyleBackColor = true;
+            this.button_resimyukle.Click += new System.EventHandler(this.button_resimyukle_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(811, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Constrast Strech";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 320);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart1.Size = new System.Drawing.Size(386, 315);
+            this.chart1.TabIndex = 23;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(415, 320);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart2.Size = new System.Drawing.Size(386, 315);
+            this.chart2.TabIndex = 24;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(820, 320);
+            this.chart3.Name = "chart3";
+            this.chart3.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart3.Size = new System.Drawing.Size(386, 315);
+            this.chart3.TabIndex = 25;
+            this.chart3.Text = "chart3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Histogram - R";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(412, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Histogram - G";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(817, 301);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Histogram - B";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(811, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Constrast Slide";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(925, 143);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 34);
+            this.textBox1.TabIndex = 30;
+            this.textBox1.Text = "Enter a value for constrast level like (-10 or 10)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1227, 651);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_resimyukle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox_odsu);
+            this.Controls.Add(this.pictureBox_orjinal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_odsu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_orjinal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox_odsu;
+        private System.Windows.Forms.PictureBox pictureBox_orjinal;
+        private System.Windows.Forms.Button button_resimyukle;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Contrast Strech/Contrast Strech/Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Contrast_Strech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button_resimyukle_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            openFileDialog1.InitialDirectory = "C:\\Users\\AdemKose\\Desktop";
+            openFileDialog1.RestoreDirectory = true;
+            pictureBox_orjinal.Image = Image.FromFile(openFileDialog1.FileName.ToString());
+            pictureBox_orjinal.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap resimorjinal = new Bitmap(pictureBox_orjinal.Image);
+            Bitmap resim_strech = new Bitmap(pictureBox_orjinal.Image);
+            int[] histogram = new int[256];
+            int[] histogram_R = new int[256];
+            int[] histogram_G = new int[256];
+            int[] histogram_B = new int[256];
+            for (int i = 0; i < pictureBox_orjinal.Image.Width; i++)
+            {
+
+                for (int j = 0; j < pictureBox_orjinal.Image.Height; j++)
+                {
+
+                    histogram[(int)(resimorjinal.GetPixel(i, j).R * 0.3 + resimorjinal.GetPixel(i, j).G * 0.59 + resimorjinal.GetPixel(i, j).B * 0.11)]++;
+                    histogram_R[resimorjinal.GetPixel(i, j).R]++;
+                    histogram_G[resimorjinal.GetPixel(i, j).G]++;
+                    histogram_B[resimorjinal.GetPixel(i, j).B]++;
+                }
+            }
+
+
+
+            //histogram ekrana basılıyor
+            Series seri_R = chart1.Series.Add("Histogram R");
+            Series seri_G = chart2.Series.Add("Histogram G");
+            Series seri_B = chart3.Series.Add("Histogram B");
+           
+            int index_enbuyuk_histogram = 0;
+            int index_enkucuk_histogram = 255;
+
+            int index_enbuyuk_histogram_R = 0;
+            int index_enkucuk_histogram_R = 255;
+
+
+            int index_enbuyuk_histogram_G = 0;
+            int index_enkucuk_histogram_G = 255;
+
+
+            int index_enbuyuk_histogram_B = 0;
+            int index_enkucuk_histogram_B = 255;
+
+            for (int i = 0; i < 256; i++)
+            {
+                seri_R.Points.Add(histogram_R[i]);
+                seri_G.Points.Add(histogram_G[i]);
+                seri_B.Points.Add(histogram_B[i]);
+
+                if (i < index_enkucuk_histogram && histogram[i] != 0)
+                {                    
+                    index_enkucuk_histogram = i;
+                }
+                if (i > index_enbuyuk_histogram && histogram[i] != 0)
+                {                  
+                    index_enbuyuk_histogram = i;
+                }
+
+
+                if (i < index_enkucuk_histogram_R && histogram_R[i] != 0)
+                {
+                    index_enkucuk_histogram_R = i;
+                }
+                if (i > index_enbuyuk_histogram_R && histogram_R[i] != 0)
+                {
+                    index_enbuyuk_histogram_R = i;
+                }
+
+                if (i < index_enkucuk_histogram_G && histogram_G[i] != 0)
+                {
+                    index_enkucuk_histogram_G = i;
+                }
+                if (i > index_enbuyuk_histogram_G && histogram_G[i] != 0)
+                {
+                    index_enbuyuk_histogram_G = i;
+                }
+
+                if (i < index_enkucuk_histogram_B && histogram_B[i] != 0)
+                {
+                    index_enkucuk_histogram_B = i;
+                }
+                if (i > index_enbuyuk_histogram_B && histogram_B[i] != 0)
+                {
+                    index_enbuyuk_histogram_B = i;
+                }
+
+
+
+
+
+
+
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                histogram[i] = 0;
+                histogram_R[i] = 0;
+                histogram_G[i] = 0;
+                histogram_B[i] = 0;
+            }
+
+            for (int i = 0; i < pictureBox_orjinal.Image.Width; i++)
+            {
+
+                for (int j = 0; j < pictureBox_orjinal.Image.Height; j++)
+                {
+
+                    Color a = new Color();
+                    a = resimorjinal.GetPixel(i, j);
+                    
+                    int Rout, Gout, Bout, Iout;
+                    int z = (int)(a.R * 0.3 + a.G * 0.59 + a.B * 0.11);
+
+                    Iout = (z - index_enkucuk_histogram) * ((255 - 0) / (index_enbuyuk_histogram - index_enkucuk_histogram)) + 0;
+
+                    Rout = (a.R - index_enkucuk_histogram_R) * ((255 - 0) / (index_enbuyuk_histogram_R - index_enkucuk_histogram_R)) + 0;
+
+                    Gout = (a.G - index_enkucuk_histogram_G) * ((255 - 0) / (index_enbuyuk_histogram_G - index_enkucuk_histogram_G)) + 0;
+
+                    Bout = (a.B - index_enkucuk_histogram_B) * ((255 - 0) / (index_enbuyuk_histogram_B - index_enkucuk_histogram_B)) + 0;
+
+
+                    histogram[Iout]++;
+                    histogram_R[Rout]++;
+                    histogram_G[Rout]++;
+                    histogram_B[Rout]++;
+
+                    resim_strech.SetPixel(i, j, Color.FromArgb( Rout, Gout, Bout));
+                }
+
+            }
+            pictureBox_odsu.Image = resim_strech;
+
+
+            //MessageBox.Show(enkucuk_histogram.ToString() + "  " + enbuyuk_histogram.ToString());
+           this.Text=(index_enkucuk_histogram.ToString() + "  " + index_enbuyuk_histogram.ToString());
+
+
+
+          
+
+
+            for (int i = 0; i < pictureBox_orjinal.Image.Width; i++)
+            {
+
+                for (int j = 0; j < pictureBox_orjinal.Image.Height; j++)
+                {
+
+                    histogram[(int)(resim_strech.GetPixel(i, j).R * 0.3 + resim_strech.GetPixel(i, j).G * 0.59 + resim_strech.GetPixel(i, j).B * 0.11)]++;
+                }
+            }
+
+
+
+            //histogram ekrana basılıyor
+
+            Series seri_R2 = chart1.Series.Add("Strech Histogram R");
+            Series seri_G2 = chart2.Series.Add("Strech Histogram G");
+            Series seri_B2 = chart3.Series.Add("Strech Histogram B");
+
+            for (int i = 0; i < 256; i++)
+            {
+                seri_R2.Points.Add(histogram_R[i]);
+                seri_G2.Points.Add(histogram_G[i]);
+                seri_B2.Points.Add(histogram_B[i]);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int kaydirma_degeri =Convert.ToInt32(textBox1.Text);
+            Bitmap resimorjinal = new Bitmap(pictureBox_orjinal.Image);
+            Bitmap resim_strech = new Bitmap(pictureBox_orjinal.Image);
+            int[] histogram = new int[256];
+            int[] histogram_R = new int[256];
+            int[] histogram_G = new int[256];
+            int[] histogram_B = new int[256];
+            for (int i = 0; i < pictureBox_orjinal.Image.Width; i++)
+            {
+
+                for (int j = 0; j < pictureBox_orjinal.Image.Height; j++)
+                {
+
+                    histogram[(int)(resimorjinal.GetPixel(i, j).R * 0.3 + resimorjinal.GetPixel(i, j).G * 0.59 + resimorjinal.GetPixel(i, j).B * 0.11)]++;
+                    histogram_R[resimorjinal.GetPixel(i, j).R]++;
+                    histogram_G[resimorjinal.GetPixel(i, j).G]++;
+                    histogram_B[resimorjinal.GetPixel(i, j).B]++;
+                }
+            }
+
+
+
+            //histogram ekrana basılıyor
+            Series seri_R = chart1.Series.Add("Histogram R");
+            Series seri_G = chart2.Series.Add("Histogram G");
+            Series seri_B = chart3.Series.Add("Histogram B");
+
+            int index_enbuyuk_histogram = 0;
+            int index_enkucuk_histogram = 255;
+
+            int index_enbuyuk_histogram_R = 0;
+            int index_enkucuk_histogram_R = 255;
+
+
+            int index_enbuyuk_histogram_G = 0;
+            int index_enkucuk_histogram_G = 255;
+
+
+            int index_enbuyuk_histogram_B = 0;
+            int index_enkucuk_histogram_B = 255;
+
+            for (int i = 0; i < 256; i++)
+            {
+                seri_R.Points.Add(histogram_R[i]);
+                seri_G.Points.Add(histogram_G[i]);
+                seri_B.Points.Add(histogram_B[i]);
+
+                if (i < index_enkucuk_histogram && histogram[i] != 0)
+                {
+                    index_enkucuk_histogram = i;
+                }
+                if (i > index_enbuyuk_histogram && histogram[i] != 0)
+                {
+                    index_enbuyuk_histogram = i;
+                }
+
+
+                if (i < index_enkucuk_histogram_R && histogram_R[i] != 0)
+                {
+                    index_enkucuk_histogram_R = i;
+                }
+                if (i > index_enbuyuk_histogram_R && histogram_R[i] != 0)
+                {
+                    index_enbuyuk_histogram_R = i;
+                }
+
+                if (i < index_enkucuk_histogram_G && histogram_G[i] != 0)
+                {
+                    index_enkucuk_histogram_G = i;
+                }
+                if (i > index_enbuyuk_histogram_G && histogram_G[i] != 0)
+                {
+                    index_enbuyuk_histogram_G = i;
+                }
+
+                if (i < index_enkucuk_histogram_B && histogram_B[i] != 0)
+                {
+                    index_enkucuk_histogram_B = i;
+                }
+                if (i > index_enbuyuk_histogram_B && histogram_B[i] != 0)
+                {
+                    index_enbuyuk_histogram_B = i;
+                }
+
+
+
+
+
+
+
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                histogram[i] = 0;
+                histogram_R[i] = 0;
+                histogram_G[i] = 0;
+                histogram_B[i] = 0;
+            }
+
+            for (int i = 0; i < pictureBox_orjinal.Image.Width; i++)
+            {
+
+                for (int j = 0; j < pictureBox_orjinal.Image.Height; j++)
+                {
+
+                    Color a = new Color();
+                    a = resimorjinal.GetPixel(i, j);
+
+                    int Rout, Gout, Bout, Iout;
+                    int z = (int)(a.R * 0.3 + a.G * 0.59 + a.B * 0.11);
+
+                    Iout = (z - index_enkucuk_histogram) * ((index_enbuyuk_histogram - index_enkucuk_histogram) / (index_enbuyuk_histogram - index_enkucuk_histogram)) +index_enkucuk_histogram+kaydirma_degeri;
+
+                    Rout = (a.R - index_enkucuk_histogram_R) * ((index_enbuyuk_histogram - index_enkucuk_histogram) / (index_enbuyuk_histogram_R - index_enkucuk_histogram_R)) + index_enkucuk_histogram + kaydirma_degeri;
+
+                    Gout = (a.G - index_enkucuk_histogram_G) * ((index_enbuyuk_histogram - index_enkucuk_histogram) / (index_enbuyuk_histogram_G - index_enkucuk_histogram_G)) + index_enkucuk_histogram + kaydirma_degeri;
+
+                    Bout = (a.B - index_enkucuk_histogram_B) * ((index_enbuyuk_histogram - index_enkucuk_histogram) / (index_enbuyuk_histogram_B - index_enkucuk_histogram_B)) + index_enkucuk_histogram + kaydirma_degeri;
+                    histogram[Iout]++;
+                    histogram_R[Rout]++;
+                    histogram_G[Rout]++;
+                    histogram_B[Rout]++;
+
+                    resim_strech.SetPixel(i, j, Color.FromArgb(Rout, Gout, Bout));
+                }
+
+            }
+            pictureBox_odsu.Image = resim_strech;
+
+
+            //MessageBox.Show(enkucuk_histogram.ToString() + "  " + enbuyuk_histogram.ToString());
+            this.Text = (index_enkucuk_histogram.ToString() + "  " + index_enbuyuk_histogram.ToString());
+
+
+
+
+
+
+            for (int i = 0; i < pictureBox_orjinal.Image.Width; i++)
+            {
+
+                for (int j = 0; j < pictureBox_orjinal.Image.Height; j++)
+                {
+
+                    histogram[(int)(resim_strech.GetPixel(i, j).R * 0.3 + resim_strech.GetPixel(i, j).G * 0.59 + resim_strech.GetPixel(i, j).B * 0.11)]++;
+                }
+            }
+
+
+
+            //histogram ekrana basılıyor
+
+            Series seri_R2 = chart1.Series.Add("Slide Histogram R");
+            Series seri_G2 = chart2.Series.Add("Slide Histogram G");
+            Series seri_B2 = chart3.Series.Add("Slide Histogram B");
+
+            for (int i = 0; i < 256; i++)
+            {
+                seri_R2.Points.Add(histogram_R[i]);
+                seri_G2.Points.Add(histogram_G[i]);
+                seri_B2.Points.Add(histogram_B[i]);
+            }
+        }
+
+
+    }
+}
